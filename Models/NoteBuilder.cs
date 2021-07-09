28c8dfe6399c87254e4cd0dcc16a453b2f351965
AlexSciencesU <a.dhorne@sciencesu-lyon.fr>
2021-07-09 10:26:48 +0200

    avancé sur le pattern builder

---
+++ Models/NoteBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesignPatternTPfinal.Models
+{
+    public abstract class NoteBuilder
+    {
+        protected Note note;
+        public Note Note
+        {
+            get { return note; }
+        }
+
+        public abstract void ConvertType();
+    }
+}
---
+++ Models/NoteProBuilder.cs
@@ -0,0 +1,18 @@
+﻿using DesignPatternTPfinal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesignPatternTPfinal.Models
+{
+    public class NoteProBuilder : NoteBuilder
+    {
+        public override void ConvertType()
+        {
+            var note = DbEntities.DbNotesEntities.Note.FirstOrDefault();
+            
+
+        }
+    }
+}
