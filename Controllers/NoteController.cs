7e62c8b37e27f94ac55af7375b338d12b5b5c73d
Rexcoffon <remi.excoffon@gmx.fr>
2021-07-09 10:48:25 +0200

    add notes in index

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DesignPatternTPfinal.Entities;
+using DesignPatternTPfinal.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,11 @@ namespace DesignPatternTPfinal.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            return View();
+            var noteModel = new NoteIndex();
+
+            noteModel.Notes = DbEntities.DbNotesEntities.Note.Take(3).Select(n => new Models.Note() { Id = n.Id, Description = n.Description, Titre = n.Titre, Date = n.Date ?? DateTime.MinValue }).ToList();
+
+            return View(noteModel);
         }
     }
 }
---
+++ Models/NoteIndex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesignPatternTPfinal.Models
+{
+    public class NoteIndex
+    {
+        public List<Note> Notes { get; set; } = new List<Note>();
+    }
+}
